9c14e3f4e46541dd5b513b96b875453bacefcfba
Franz <2simple233@gmail.com>
2022-08-16 05:46:34 +0800

    61a lab10

--- CS61A/lab/lab10/lab10.scm
+++ CS61A/lab/lab10/lab10.scm
@@ -6,25 +6,31 @@
   (cons name (cons class students)))
 
 (define (student-get-name student)
-  'YOUR-CODE-HERE)
+  (car student))
 
 (define (student-get-classes student)
-  'YOUR-CODE-HERE)
+  (cdr student))
 
 (define (teacher-get-name teacher)
-  'YOUR-CODE-HERE)
+  (car teacher))
 
 (define (teacher-get-class teacher)
-  'YOUR-CODE-HERE)
+ (car (cdr teacher)))
 
 (define (teacher-get-students teacher)
-  'YOUR-CODE-HERE)
+(cdr (cdr teacher)))
 
 (define (student-attend-class student class)
-  'YOUR-CODE-HERE)
+(student-create (student-get-name student) (cons class (student-get-classes student))))
 
 (define (teacher-hold-class teacher)
-  'YOUR-CODE-HERE)
+
+  (define class (teacher-get-class teacher))
+  (define new_students (map (lambda (x) (student-attend-class x class))
+  (teacher-get-students teacher)))
+
+  (teacher-create (teacher-get-name teacher) class new_students)
+)
 
 ; Rational Abstraction
 ; Helpers
