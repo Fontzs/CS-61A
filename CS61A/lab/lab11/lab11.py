2f787336e5ef261707511ad08d4206761b2f223e
Franz <2simple233@gmail.com>
2022-08-18 19:50:03 +0800

    61a lab11

--- CS61A/lab/lab11/lab11.py
+++ CS61A/lab/lab11/lab11.py
@@ -25,10 +25,7 @@ def scientific_name(name):
     >>> scientific_name("I want a pet T. rex right now")
     False
     """
-    return bool(re.search(__________, name))
-
-
-import re
+    return bool(re.search(r"^[A-Z][\.a-z]{1}[a-z]*\s[a-z]+$", name))
 
 
 def calculator_ops(calc_str):
@@ -49,4 +46,4 @@ def calculator_ops(calc_str):
     >>> calculator_ops("+ 3 23")
     False
     """
-    return bool(re.search(__________, calc_str))
+    return bool(re.search(r"\(([-+*/]\s+\d+\s+\d+)\)", calc_str))
